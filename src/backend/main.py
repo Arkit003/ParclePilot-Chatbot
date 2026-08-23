3f07e6892a615be2223cd8dbb8ebd2a88a75cbec
Arkit003 <arkit312003@gmail.com>
2026-08-23 21:24:39 +0530

    some changes to the cors for deployment

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import os
 import logging
 from contextlib import asynccontextmanager
 
@@ -52,16 +52,23 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+
+
+
+
+
+FRONTEND_URL = os.getenv(
+    "FRONTEND_URL",
+    "http://localhost:5173",
+)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-    ],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(
     health_router,
 )
